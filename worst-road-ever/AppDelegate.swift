893956f5c15180fed8ecfc0dfcb5d309c8d660e4
fabian <fabian.karl@cobi.bike>
2020-04-29 15:02:36 +0200

    test changes

--- worst-road-ever/AppDelegate.swift
+++ worst-road-ever/AppDelegate.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import CoreMotion
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let motion: CMMotionManager = CMMotionManager()
+    var timer: Timer = Timer()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,6 +32,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func startAccelerometers() {
+       // Make sure the accelerometer hardware is available.
+       if self.motion.isAccelerometerAvailable {
+          self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
+          self.motion.startAccelerometerUpdates()
+
+          // Configure a timer to fetch the data.
+          self.timer = Timer(fire: Date(), interval: (1.0/60.0),
+                repeats: true, block: { (timer) in
+             // Get the accelerometer data.
+             if let data = self.motion.accelerometerData {
+                let x = data.acceleration.x
+                let y = data.acceleration.y
+                let z = data.acceleration.z
+
+                // Use the accelerometer data in your app.
+             }
+          })
+
+          // Add the timer to the current run loop.
+          RunLoop.current.add(self.timer, forMode: RunLoop.Mode.default)
+       }
     }
 
 
