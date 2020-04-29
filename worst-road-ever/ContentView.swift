2ea5c37a147c1e3460400fbab8356f16cea4d281
fabian <fabian.karl@cobi.bike>
2020-04-29 14:48:12 +0200

    first commit

---
+++ worst-road-ever/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  worst-road-ever
+//
+//  Created by Fabian Karl on 29.04.20.
+//  Copyright © 2020 Fabian Karl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ worst-road-everTests/worst_road_everTests.swift
@@ -0,0 +1,34 @@
+//
+//  worst_road_everTests.swift
+//  worst-road-everTests
+//
+//  Created by Fabian Karl on 29.04.20.
+//  Copyright © 2020 Fabian Karl. All rights reserved.
+//
+
+import XCTest
+@testable import worst_road_ever
+
+class worst_road_everTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
